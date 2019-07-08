4024fb6cf772b8c51eb1910d188f2c161a459848
Art Jazmines <art.jazmines@xuenn.com>
2019-07-08 19:10:56 +0800

    Cleaned up HTML and added Webpack Server

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # webpack-basic
 Basic implementation of webpack with inline SVG, SASS, image asset support
+
+#Be sure to run npm install on terminal upon first download
+
+Use the following command on terminal
+
+npm run prod
+
+or 
+
+npm run dev
--- webpack.config.js
+++ webpack.config.js
@@ -4,74 +4,79 @@ const nodeExternals = require('webpack-node-externals');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require("webpack-md5-hash");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 
-module.exports = {
-    entry: {
-        main: './src/index.js',
-        common: './src/style.scss'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
-    },
-    target: 'node',
-    externals: [nodeExternals()],
-    module: {
-        rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/,
-                use: [
-                    {
-                      loader: 'file-loader',
-                      options: {
-                        name: '[path][name].[ext]',
-                      },
-                    },
-                  ]
-            },
-            {
-                test: /\.(svg)$/,
-                use: [
-                    'svg-url-loader',
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        }),
-        new HtmlWebpackPlugin({
-            inject: false,
-            hash: true,
-            template: './src/index.html',
-            filename: 'index.html'
-        }),
-        new HtmlWebpackPlugin({
-            inject: false,
-            hash: true,
-            template: './src/page.html',
-            filename: 'page.html'
-        }),
-        new WebpackMd5Hash(),
-
-    ]
+module.exports = env => {
+    console.log('Production',env.production);
+    return {
+        entry: {
+            main: './src/index.js',
+            common: './src/style.scss'
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'public/scripts/[name].js'
+        },
+        target: 'node',
+        externals: [nodeExternals()],
+        module: {
+            rules: [{
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        'style-loader',
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader',
+                        'sass-loader'
+                    ],
+                },
+                {
+                    test: /\.(eot|ttf|woff|woff2|png|jpg|gif|svg|ico)$/,
+                    use: [{
+                        loader: 'url-loader',
+                        options: {
+                            name: '[name].[ext]?v=[hash:8]',
+                            //size lower than 8KB convert file to base64
+                            limit: 1,
+                            publicPath:  env.production ? 'public/images/':'../../public/images/',
+                            outputPath: './public/images/'
+                        }
+                    }]
+                },
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: 'public/css/[name].css',
+            }),
+            new HtmlWebpackPlugin({
+                inject: false,
+                hash: true,
+                template: './src/index.html',
+                filename: 'index.html'
+            }),
+            new HtmlWebpackPlugin({
+                inject: false,
+                hash: true,
+                template: './src/page.html',
+                filename: 'page.html'
+            }),
+            new WebpackMd5Hash(), //Help organize the @media styles
+        ],
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 9000
+        }
+    }
 };
