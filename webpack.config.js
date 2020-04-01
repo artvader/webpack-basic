2c479c265e396b6e301297856a532b880177f11e
Art <art@larcandasset.com>
2020-04-01 16:05:12 +0800

    Updated Basic Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,98 @@
-// webpack v4
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackMd5Hash = require("webpack-md5-hash");
-const {
-    CleanWebpackPlugin
-} = require('clean-webpack-plugin');
+// This is main configuration object.
+// Here you write different options and tell Webpack what to do
+module.exports = {
+  // Path to your entry point. From this file Webpack will begin his work
+  entry: './src/javascript/index.js',
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on final bundle. For now we don't need production's JavaScript 
+  // minifying and other thing so let's set mode to development
+  mode: 'development',
 
-module.exports = env => {
-    console.log('Production',env.production);
-    return {
-        entry: {
-            main: './src/index.js',
-            common: './src/style.scss'
-        },
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'public/scripts/[name].js'
-        },
-        target: 'node',
-        externals: [nodeExternals()],
-        module: {
-            rules: [{
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader"
-                    }
-                },
-                {
-                    test: /\.scss$/,
-                    use: [
-                        'style-loader',
-                        MiniCssExtractPlugin.loader,
-                        'css-loader',
-                        'postcss-loader',
-                        'sass-loader'
-                    ],
-                },
-                {
-                    test: /\.(eot|ttf|woff|woff2|png|jpg|gif|svg|ico)$/,
-                    use: [{
-                        loader: 'url-loader',
-                        options: {
-                            name: '[name].[ext]?v=[hash:8]',
-                            //size lower than 8KB convert file to base64
-                            limit: 1,
-                            publicPath:  env.production ? 'public/images/':'../../public/images/',
-                            outputPath: './public/images/'
-                        }
-                    }]
-                },
-            ]
-        },
-        plugins: [
-            new CleanWebpackPlugin(),
-            new MiniCssExtractPlugin({
-                filename: 'public/css/[name].css',
-            }),
-            new HtmlWebpackPlugin({
-                inject: false,
-                hash: true,
-                template: './src/index.html',
-                filename: 'index.html'
-            }),
-            new HtmlWebpackPlugin({
-                inject: false,
-                hash: true,
-                template: './src/page.html',
-                filename: 'page.html'
-            }),
-            new WebpackMd5Hash(), //Help organize the @media styles
-        ],
-        devServer: {
-            contentBase: path.join(__dirname, 'dist'),
-            compress: true,
-            port: 9000
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
-    }
+      },
+      {
+        // Apply rule for .sass, .scss or .css files
+        test: /\.(sa|sc|c)ss$/,
+        // Set loaders to transform files.
+        // Loaders are applying from right to left(!)
+        // The first loader will be applied after others
+        use: [
+          {
+            // After all CSS loaders we use plugin to do his work.
+            // It gets all transformed CSS and extracts it into separate
+            // single bundled file
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            // This loader resolves url() and @imports inside CSS
+            loader: "css-loader",
+          },
+          {
+            // Then we apply postCSS fixes like autoprefixer and minifying
+            loader: "postcss-loader"
+          },
+          {
+            // First we transform SASS to standard CSS
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ]
+      },
+      {
+        // Now we apply rule for images
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            // Using file-loader for these files
+            loader: "file-loader",
+            // In options we can set different things like format
+            // and directory to save
+            options: {
+              outputPath: 'images'
+            }
+          }
+        ]
+      },
+      {
+        // Apply rule for fonts files
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            // Using file-loader too
+            loader: "file-loader",
+            options: {
+              outputPath: 'fonts'
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    })
+  ]
 };
